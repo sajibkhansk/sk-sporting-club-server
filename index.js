358194543b9349e060a81530be0a9c1879d50429
sajibkhansk <sajibkhan117593@gmial.com>
2023-06-11 13:10:17 +0600

    patch admin

--- index.js
+++ index.js
@@ -23,10 +23,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-
+    const userCollection = client.db("sk-sports").collection("users");
     const classesCollection = client.db("sk-sports").collection("classes");
     const cartCollection = client.db("sk-sports").collection("carts");
+//user related apis
+app.get("/users", async (req, res) => {
+  const result = await userCollection.find({}).toArray()
+  res.send(result);
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = req.body;
+    const query = {email: user.email}
+    const preUser = await userCollection.findOne(query);
+    if(preUser){
+      return res.send({m: "exist"})
+    }
+    const result = await userCollection.insertOne(user);
+    res.send({ insertedId: result.insertedId });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error creating user');
+  }
+});
+
+app.patch('/users/admin/:id', async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      role: 'admin'
+    },
+  };
+
+  const result = await userCollection.updateOne(filter, updateDoc);
+  res.send(result);
+
+})
+
     //  { GET ALL CLASSES} 
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find({}).toArray()
@@ -51,11 +87,18 @@ async function run() {
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+
+    });
     //creating connetction with beckend
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });            
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
